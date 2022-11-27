1b7e0fba54ab47cb580ddab30d48c32c95739e48
Yeozen <yeozen4@gmail.com>
2022-11-27 21:27:58 +0800

    Menu, color and board size picker
    Added a start menu, an option to pick the game background color and board size (3x3, 4x4, 5x5)

--- src/src/main/java/com/example/demo/GameScene.java
+++ src/src/main/java/com/example/demo/GameScene.java
@@ -13,7 +13,7 @@ import java.util.Random;
 
 class GameScene {
     private static int HEIGHT = 600;
-    private static int n = 4;
+    public static int n = 4;
     private final static int distanceBetweenCells = 10;
     private static double LENGTH = (HEIGHT - ((n + 1) * distanceBetweenCells)) / (double) n;
     private TextMaker textMaker = TextMaker.getSingleInstance();
