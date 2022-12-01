8fce07f8646a43cf9ad880d08bc6e61060eb230a
Yeozen <yeozen4@gmail.com>
2022-12-01 22:50:50 +0800

    Cleanup
    Removed a lot of unused imports

--- src/src/main/java/com/example/demo/Main.java
+++ src/src/main/java/com/example/demo/Main.java
@@ -1,24 +1,12 @@
 package com.example.demo;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.ButtonType;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.shape.Rectangle;
 
-import java.io.IOException;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Scanner;
 
 public class Main extends Application {
