1e746e384e4062f847a831fe08b6091d214f7b78
Yeozen <yeozen4@gmail.com>
2022-12-06 01:13:24 +0800

    return to menu button
    tried to implement return to menu function here, still not working

--- src/src/main/java/com/example/demo/LeaderboardController.java
+++ src/src/main/java/com/example/demo/LeaderboardController.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
@@ -15,6 +18,9 @@ import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LeaderboardController implements Initializable {
+    @FXML
+    private Button returntomenu;
+
     @FXML
     private List<Label> labelList;
 
@@ -25,5 +31,16 @@ public class LeaderboardController implements Initializable {
 
     public final Label getLabel(int i){
         return labelList.get(i);
+    }
+
+    public void returntomenu() throws IOException {
+        Stage leaderboard_stage = (Stage) returntomenu.getScene().getWindow();
+        leaderboard_stage.close();
+        Parent pane;
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        pane = loader.load();
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(new Scene(pane,600,500));
     }
 }
