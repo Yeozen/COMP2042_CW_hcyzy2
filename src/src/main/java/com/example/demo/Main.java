64944fcbf5a309a5cd0846b00f2f4828235260e6
Yeozen <yeozen4@gmail.com>
2022-12-09 18:35:10 +0800

    Update Main.java

--- src/src/main/java/com/example/demo/Main.java
+++ src/src/main/java/com/example/demo/Main.java
@@ -1,31 +1,19 @@
 package com.example.demo;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.ButtonType;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.shape.Rectangle;
 
 import java.util.Objects;
-import java.util.Optional;
-import java.util.Scanner;
 
 public class Main extends Application {
-    private static Scanner input= new Scanner(System.in);
+    
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
+        Parent root; = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
