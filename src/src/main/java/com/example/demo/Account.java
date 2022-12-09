a5b595e4687d15b480a4eb162b617d1feb1cf1af
Yeozen <yeozen4@gmail.com>
2022-12-09 18:30:49 +0800

    duplicate check update
    updated the duplicate check function because it wasn't working for some edge cases.

--- src/src/main/java/com/example/demo/Account.java
+++ src/src/main/java/com/example/demo/Account.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import javafx.scene.Group;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -14,7 +12,6 @@ import static com.example.demo.GameScene.score;
 
 public class Account {
 
-    private String userName;
     private static ArrayList<Account> accounts = new ArrayList<>();
     public static String[][] LeaderboardArray = new String[11][2];
     private static boolean isDuplicate = false;
@@ -23,22 +20,9 @@ public class Account {
         return score;
     }
 
-    private String getUserName() {
-        return userName;
-    }
-
-    static Account accountHaveBeenExist(String userName) {
-        for (Account account : accounts) {
-            if (account.getUserName().equals(userName)) {
-                return account;
-            }
-        }
-        return null;
-
-    }
 
     static int lengthOfFile() throws IOException {
-        FileReader fr = new FileReader("\\C:\\GitHub\\COMP2042_CW_hcyzy2\\src\\src\\main\\java\\com\\example\\demo\\scores.txt");
+        FileReader fr = new FileReader("scores.txt");
         BufferedReader br = new BufferedReader(fr);
         String line;
         int lengthOfFile = 0;
@@ -49,7 +33,7 @@ public class Account {
     }
 
     static void populateArray() throws IOException {
-        FileReader fr = new FileReader("\\C:\\GitHub\\COMP2042_CW_hcyzy2\\src\\src\\main\\java\\com\\example\\demo\\scores.txt");
+        FileReader fr = new FileReader("scores.txt");
         BufferedReader br = new BufferedReader(fr);
         String line;
         for (int i = 0; i < lengthOfFile() / 2; i++) {
@@ -60,22 +44,25 @@ public class Account {
             }
         }
         br.close();
-        System.out.println(Arrays.deepToString(LeaderboardArray));
     }
 
     static void saveScore() throws IOException {
-        FileWriter fw = new FileWriter("\\C:\\GitHub\\COMP2042_CW_hcyzy2\\src\\src\\main\\java\\com\\example\\demo\\scores.txt", true);
+        FileWriter fw = new FileWriter("scores.txt", true);
         fw.append(Controller.userName).append("\n").append(String.valueOf(getScore())).append("\n");
         fw.close();
         populateArray();
         duplicateCheck();
         sortArray();
-        FileWriter sortedScores = new FileWriter("\\C:\\GitHub\\COMP2042_CW_hcyzy2\\src\\src\\main\\java\\com\\example\\demo\\scores.txt");
-        for (int i = 0; i < 10; i++) {
+        int lof = lengthOfFile();
+        FileWriter sortedScores = new FileWriter("scores.txt");
+        int arrayLimit = 0;
+        if (isDuplicate) {
+            arrayLimit += 1;
+        }
+        for (int i = 0; i < (lof/2) - arrayLimit; i++) {
             for (int j = 0; j < 2; j++) {
                 if (LeaderboardArray[i][j] != null) {
                     sortedScores.write(LeaderboardArray[i][j] + "\n");
-                    System.out.println(LeaderboardArray[i][j]);
                 }
             }
         }
@@ -83,21 +70,7 @@ public class Account {
     }
 
     static void sortArray() throws IOException {
-        if (isDuplicate) {
-            for (int k = 0; k < (lengthOfFile() / 2) - 2; k++) {
-                for (int i = 0; i < (lengthOfFile() / 2) - 2; i++) {
-                    if (Integer.parseInt(LeaderboardArray[i][1]) < Integer.parseInt(LeaderboardArray[i + 1][1])) {
-                        String tempScore = LeaderboardArray[i][1];
-                        String tempName = LeaderboardArray[i][0];
-                        LeaderboardArray[i][1] = LeaderboardArray[i + 1][1];
-                        LeaderboardArray[i][0] = LeaderboardArray[i + 1][0];
-                        LeaderboardArray[i + 1][1] = tempScore;
-                        LeaderboardArray[i + 1][0] = tempName;
-                    }
-                }
-            }
-        } else {
-            for (int k = 0; k < (lengthOfFile() / 2) - 1; k++) {
+        for (int k = 0; k < (lengthOfFile() / 2) - 1; k++) {
                 for (int i = 0; i < (lengthOfFile() / 2) - 1; i++) {
                     if (Integer.parseInt(LeaderboardArray[i][1]) < Integer.parseInt(LeaderboardArray[i + 1][1])) {
                         String tempScore = LeaderboardArray[i][1];
@@ -108,19 +81,23 @@ public class Account {
                         LeaderboardArray[i + 1][0] = tempName;
                     }
                 }
-            }
         }
     }
 
-    static boolean duplicateCheck() throws IOException {
+    static void duplicateCheck() throws IOException {
         for (int i = 0; i < (lengthOfFile() / 2) - 1; i++) {
             if (Objects.equals(LeaderboardArray[i][0], Controller.userName)){
                 if (Integer.parseInt(LeaderboardArray[i][1]) < getScore()){
                     LeaderboardArray[i][1] = String.valueOf(getScore());
-                    isDuplicate = true;
+                    String tempScore = LeaderboardArray[((lengthOfFile() / 2) - 1)][1];
+                    String tempName = LeaderboardArray[((lengthOfFile() / 2) - 1)][0];
+                    LeaderboardArray[((lengthOfFile() / 2) - 1)][1] = LeaderboardArray[i][1];
+                    LeaderboardArray[((lengthOfFile() / 2) - 1)][0] = LeaderboardArray[i][0];
+                    LeaderboardArray[i][1] = tempScore;
+                    LeaderboardArray[i][0] = tempName;
                 }
+                isDuplicate = true;
             }
         }
-        return isDuplicate;
     }
 }
