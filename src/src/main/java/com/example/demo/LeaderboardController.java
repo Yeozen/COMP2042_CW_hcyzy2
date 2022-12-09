2409e0d436770c6f2913eafccdf21e1c91541012
Yeozen <yeozen4@gmail.com>
2022-12-09 18:34:36 +0800

    close button
    close button

--- src/src/main/java/com/example/demo/LeaderboardController.java
+++ src/src/main/java/com/example/demo/LeaderboardController.java
@@ -1,23 +1,21 @@
 package com.example.demo;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
+import static com.example.demo.Main.root;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LeaderboardController implements Initializable {
+    static Group menuRoot;
     @FXML
     private Button returntomenu;
 
@@ -33,14 +31,12 @@ public class LeaderboardController implements Initializable {
         return labelList.get(i);
     }
 
+    public static Parent getRoot() {
+        return root;
+    }
+
     public void returntomenu() throws IOException {
         Stage leaderboard_stage = (Stage) returntomenu.getScene().getWindow();
         leaderboard_stage.close();
-        Parent pane;
-        final FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
-        pane = loader.load();
-        Stage stage = new Stage();
-        stage.initStyle(StageStyle.UNDECORATED);
-        stage.setScene(new Scene(pane,600,500));
     }
 }
