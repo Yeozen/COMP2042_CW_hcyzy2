18005649ec06ec68c210362cc39bea90ef4cdae7
Yeozen <yeozen4@gmail.com>
2022-12-09 18:32:08 +0800

    no name check
    added a alert to prevent users from entering no name

--- src/src/main/java/com/example/demo/Controller.java
+++ src/src/main/java/com/example/demo/Controller.java
@@ -4,145 +4,123 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-<<<<<<< Updated upstream
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-=======
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.*;
 import javafx.stage.Modality;
->>>>>>> Stashed changes
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Optional;
 
-<<<<<<< Updated upstream
-=======
 import static com.example.demo.Account.*;
 
->>>>>>> Stashed changes
 
 public class Controller {
     static final int WIDTH = 900;
     static final int HEIGHT = 700;
-    private Group gameRoot = new Group();
+    private static Group gameRoot = new Group();
     private Scene gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));
-<<<<<<< Updated upstream
-    Group endgameRoot = new Group();
-=======
     private static Group leaderboardRoot = new Group();
-    private Scene leaderboardScene = new Scene(leaderboardRoot, WIDTH, HEIGHT, Color.rgb(1, 177, 92));
     public Group endgameRoot = new Group();
->>>>>>> Stashed changes
     Scene endGameScene = new Scene(endgameRoot, WIDTH, HEIGHT, Color.rgb(250, 20, 100, 0.2));
 
     @FXML
-    private Pane pane1;
+    public static Pane pane1;
 
     @FXML
     private ChoiceBox<String> colour_picker;
 
     @FXML
+    private TextField user_name;
+
+    @FXML
     private ChoiceBox<String> game_type;
 
-    private String colour_picked;
-    private String board_size;
-
+    @FXML
     public Button StartButton;
 
-    public void setGameRoot(Group gameRoot) {
-        this.gameRoot = gameRoot;
+    @FXML
+    public Button LeaderboardButton;
+
+    public static String colour_picked;
+    public static String board_size;
+    public static String userName;
+
+    public static void setGameRoot(Group gameRoot) {
+        Controller.gameRoot = gameRoot;
     }
 
     public void setGameScene(Scene gameScene) {
         this.gameScene = gameScene;
     }
 
-<<<<<<< Updated upstream
-    public void handleButtonClick(ActionEvent event){
-=======
-    public static void setLeaderboardRoot(Group gameRoot) {
-        Controller.leaderboardRoot = leaderboardRoot;
-    }
-
-    public void setLeaderboardScene(Scene gameScene) {
-        this.leaderboardScene = leaderboardScene;
-    }
-
-
     public void startGame(){
->>>>>>> Stashed changes
-
-        Rectangle backgroundOfMenu = new Rectangle(240, 120, Color.rgb(120, 120, 120, 0.2));
-        backgroundOfMenu.setX(WIDTH / 2 - 120);
-        backgroundOfMenu.setY(180);
-
-        Rectangle backgroundOfMenuForPlay = new Rectangle(240, 140, Color.rgb(120, 20, 100, 0.2));
-        backgroundOfMenuForPlay.setX(WIDTH / 2 - 120);
-        backgroundOfMenuForPlay.setY(180);
-
-        Stage game_stage = new Stage();
-        game_stage.setTitle("2048");
-        Group gameRoot = new Group();
-        setGameRoot(gameRoot);
-        System.out.println("cocksucker: "+colour_picked);
-
-        if (Objects.equals(colour_picked, "YELLOW")){
-            gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));
-        } else if (Objects.equals(colour_picked, "RED")) {
-            gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(125, 20, 0));
-        } else if (Objects.equals(colour_picked, "PURPLE")) {
-            gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(100, 100, 255));
+        userName = user_name.getText();
+        if (Objects.equals(userName, "")){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("No name warning");
+            alert.setHeaderText("No name");
+            alert.setContentText("You have not given a name, please input a name.");
+            Optional<ButtonType> result = alert.showAndWait();
         } else {
-            gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));
-        }
+            Rectangle backgroundOfMenu = new Rectangle(240, 120, Color.rgb(120, 120, 120, 0.2));
+            backgroundOfMenu.setX(WIDTH / 2 - 120);
+            backgroundOfMenu.setY(180);
 
-        if (Objects.equals(board_size, "3x3")){
-            GameScene.setN(3);
-        } else if (Objects.equals(board_size, "4x4")){
-            GameScene.setN(4);
-        } else if (Objects.equals(board_size, "5x5")){
-            GameScene.setN(5);
-        } else {
-            GameScene.setN(4);
+            Rectangle backgroundOfMenuForPlay = new Rectangle(240, 140, Color.rgb(120, 20, 100, 0.2));
+            backgroundOfMenuForPlay.setX(WIDTH / 2 - 120);
+            backgroundOfMenuForPlay.setY(180);
+
+            Stage game_stage = new Stage();
+            game_stage.setTitle("2048");
+            Group gameRoot = new Group();
+            setGameRoot(gameRoot);
+
+            if (Objects.equals(colour_picked, "YELLOW")){
+                gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));
+            } else if (Objects.equals(colour_picked, "RED")) {
+                gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(125, 28, 0));
+            } else if (Objects.equals(colour_picked, "PURPLE")) {
+                gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(100, 100, 255));
+            } else {
+                gameScene = new Scene(gameRoot, WIDTH, HEIGHT, Color.rgb(189, 177, 92));
+            }
+
+            if (Objects.equals(board_size, "3x3")){
+                GameScene.setN(3);
+            } else if (Objects.equals(board_size, "4x4")){
+                GameScene.setN(4);
+            } else if (Objects.equals(board_size, "5x5")){
+                GameScene.setN(5);
+            } else {
+                GameScene.setN(4);
+            }
+            setGameScene(gameScene);
+            game_stage.setScene(gameScene);
+            GameScene game = new GameScene();
+            game.game(gameScene, gameRoot, game_stage, endGameScene, endgameRoot);
+            Stage menu_stage = (Stage) StartButton.getScene().getWindow();
+            menu_stage.close();
+            game_stage.show();
         }
-        setGameScene(gameScene);
-        game_stage.setScene(gameScene);
-        GameScene game = new GameScene();
-        game.game(gameScene, gameRoot, game_stage, endGameScene, endgameRoot);
-        Stage menu_stage = (Stage) StartButton.getScene().getWindow();
-        menu_stage.close();
-        game_stage.show();
     }
 
     @FXML
-    void pick_colour(ActionEvent event) {
+    void pick_colour() {
         colour_picked = colour_picker.getValue();
     }
 
     @FXML
-    void set_gametype(ActionEvent event) {
+    void set_gametype() {
         board_size = game_type.getValue();
-<<<<<<< Updated upstream
-        if (Objects.equals(board_size, "3x3")){
-            GameScene.setN(3);
-        } else if (Objects.equals(board_size, "4x4")){
-            GameScene.setN(4);
-        } else if (Objects.equals(board_size, "5x5")){
-            GameScene.setN(5);
-        } else {
-            GameScene.setN(4);
-        }
-=======
     }
 
     public void show_leaderboard(ActionEvent event) throws IOException {
@@ -152,8 +130,8 @@ public class Controller {
         final FXMLLoader loader = new FXMLLoader(getClass().getResource("leaderboard.fxml"));
         pane = loader.load();
         Stage stage = new Stage();
-        stage.setScene(new Scene(pane,600,900));
-        //pane1.getChildren().setAll(pane);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(new Scene(pane,600,500));
         for (int i = 0; i < 10; i++){
             for(int j = 0; j < 2; j++){
                 ((LeaderboardController) loader.getController()).getLabel(label).setText(LeaderboardArray[i][j]);
@@ -162,14 +140,5 @@ public class Controller {
             }
         }
         stage.show();
-        /*Stage leaderboard_stage = new Stage();
-        leaderboard_stage.setTitle("leaderboard");
-        setLeaderboardRoot(leaderboardRoot);
-        setLeaderboardScene(leaderboardScene);
-        leaderboard_stage.setScene(leaderboardScene);
-        Account leaderboard = new Account();
-        leaderboard.leaderboard(leaderboardScene, leaderboardRoot, leaderboard_stage);
-        leaderboard_stage.show();*/
->>>>>>> Stashed changes
     }
 }
